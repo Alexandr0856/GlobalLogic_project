c1ecd0e0d73094e19fc87f88f37bc6a62d1b2d6f
Olexandr <ppos19soo@gmail.com>
2022-10-18 19:29:02 +0200

    correction fist task

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,38 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText main_input;
+    public static final String MAIN_MES = "mainMes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context = getApplicationContext();
 
-        main_input = findViewById(R.id.main_input);
         Button main_btn = findViewById(R.id.main_btn);
 
-        main_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Context context = getApplicationContext();
-                int duration = Toast.LENGTH_SHORT;
+        main_btn.setOnClickListener(view -> showText(context));
+    }
 
-                if (main_input.getText().toString().equals(""))
-                    Toast.makeText(context, R.string.void_input, duration).show();
-                else{
-                    String input_text = main_input.getText().toString();
-                    Toast.makeText(context, input_text, duration).show();
-                    Log.v("main_mes", input_text);
-                }
-            }
-        });
+    /***
+     *  Output text from EditText to logs and display Toast when button is clicked
+     */
+    private void showText(Context context) {
+        int duration = Toast.LENGTH_SHORT;
+
+        EditText main_input = findViewById(R.id.main_input);
+        String textToShow = main_input.getText().toString();
+
+        if (TextUtils.isEmpty(textToShow)) {
+            textToShow = String.valueOf(R.string.void_input);
+        }
+
+        Toast.makeText(context, textToShow, duration).show();
+        Log.v(MAIN_MES, textToShow);
     }
 }
